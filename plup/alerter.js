0b9712b5aaabaf9e42f4b1b8f51b43e0fb9c1c57
Aworldc <83331712+Aworldc@users.noreply.github.com>
2022-12-04 13:26:22 +1100

    Update alerter.js

--- plup/alerter.js
+++ plup/alerter.js
@@ -1,6 +1,6 @@
 // let query = window.location.search
 // let queryparser = new URLSearchParams(query)
 
-let win = window.open("https://www.google.com/")
+window.parent.alert("Test")
 
 // window.alert(queryparser.get('text'))
